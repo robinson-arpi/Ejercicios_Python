e6afc47e0693c673ec7c4c333d9141be13bdad04
robinson-arpi <robinson.arpi@gmail.com>
2023-03-23 21:22:42 -0500

    Corrección Find

--- FindEvenIndex.py
+++ FindEvenIndex.py
@@ -1,8 +1,8 @@
 #Training on Equal Sides Of An Array | Codewars
+#https://www.codewars.com/kata/5679aa472b8f57fb8c000047/train/python
+
 def find_even_index(s):
     for i in range(len(s)):
-        sum1 = sum(s[:i])
-        sum2 = sum(s[i+1:])
-        if sum1 == sum2:
+        if sum(s[:i]) == sum(s[i+1:]):
             return i
     return -1
