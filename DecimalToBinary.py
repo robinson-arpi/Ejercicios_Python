b3124b4f89b59bd84d110d4c00b785eff66b80f2
robinson-arpi <robinson.arpi@gmail.com>
2023-03-23 21:24:55 -0500

    Aún no solucioando

---
+++ DecimalToBinary.py
@@ -0,0 +1,15 @@
+'''
+Convert a number to a binary coded decimal (BCD).
+
+You can assume input will always be an integer. If it is a negative number then simply place a minus sign in front of the output string. Output will be a string with each digit of the input represented as 4 bits (0 padded) with a space between each set of 4 bits.
+
+Ex.
+
+n =  10 -> "0001 0000"
+n = -10 -> "-0001 0000"
+
+'''
+
+def binary(decimal):
+    bin(decimal)
+    
